6b3d700d95a5000a96e44bdf752d8fc8b342f551
yogeshindybots <yogesh.indybots@gmail.com>
2015-02-04 18:19:59 +0530

    added index file to each folder

---
+++ js/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>404 Not Found</title>
+<style>
+body{
+	background:#d9f8fd;
+	padding-top:100px;
+	}
+body img{
+	user-drag:none;
+	-webkit-user-drag:none;
+	-moz-user-drag:none;
+	-ms-user-drag:none;
+	-o-user-drag:none;
+	}
+</style>
+<link rel="icon" type="image/png" href="../favicon.png">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+
+<body oncontextmenu="return false">
+<div class="col-md-3"></div>
+<div class="col-md-6 text-center">
+<img src="../img/indybots.png" class="img-respoinsive" />
+<h1>404 Error</h1>
+<h3>Requested <strong>URL/js</strong> was not found on this server.</h3>
+</div>
+</body>
+</html>
